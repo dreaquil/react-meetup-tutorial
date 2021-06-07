a9f528d1a4586b497a1e23dfa9d0409006a8191a
André Aquilina <dre.aquil.94@gmail.com>
2021-06-07 23:13:07 +0100

    Add basic routes for dummy pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import Favourites from "./pages/Favourites";
+
 function App(): JSX.Element {
   return (
-    <div className="App">
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
+      </Switch>
     </div>
   );
 }
