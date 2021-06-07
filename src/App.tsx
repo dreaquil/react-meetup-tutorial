5f6ab09c6451ef963be47e9bc2a22e8268947e69
André Aquilina <dre.aquil.94@gmail.com>
2021-06-07 23:19:54 +0100

    Add basic navigation component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App(): JSX.Element {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
