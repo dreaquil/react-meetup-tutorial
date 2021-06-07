7cfbf484709cdc232095a6fccd9dc9bd23cb70ab
André Aquilina <dre.aquil.94@gmail.com>
2021-06-07 23:35:28 +0100

    Add meetup item component.

---
+++ src/components/meetups/MeetupItem.tsx
@@ -0,0 +1,28 @@
+import classes from "./MeetupItem.module.css";
+
+interface Props {
+  image: string;
+  title: string;
+  address: string;
+  description: string;
+}
+
+function MeetupItem(props: Props): JSX.Element {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favourites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
